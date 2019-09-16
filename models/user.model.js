d79bc0eeea2baf4585298037579fb50f53922845
root <root@vps259538.vps.ovh.ca>
2019-09-16 15:16:03 -0400

    refac(user.model): remove a propriedade state e adiciona a propriedade token

--- models/user.model.js
+++ models/user.model.js
@@ -13,9 +13,8 @@ const userSchema = new mongoose.Schema({
     projectId: {
         type: String
     },
-    state: {
-        type: String,
-        enum: ['neutral', 'awaitingProjectId',]
+    token: {
+        type: String
     }
 })
 
