cc62cdc0f3ff93f0e3d202fffb634aa91c6a9890
root <root@vps259538.vps.ovh.ca>
2019-09-16 13:42:03 -0400

    refac(server.js): encapsulamento de logicas nao pertecentes ao server

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
+const config = require('config');
 const express = require('express');
 const winston = require('winston');
-const app = express();
-
-const registerUser = require('./middleware/telegraf/registerUser');
-
-const config = require('config');
 const Telegraf = require('telegraf/telegraf');
+
 const bot = new Telegraf(config.get('token'));
-
-bot.use(registerUser);
-
-let state = {};
+const app = express();
 
 require('./startup/logging')();
 require('./startup/database')();
 require('./startup/routes')(app);
+require('./startup/telegram')(bot);
 require('./startup/commands')(bot);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Api rodando na porta ${port}`));
-bot.startPolling();
+
 
 module.exports = server;
 
