3afbb984e339270eef356be169fb791fbc6216ff
root <root@vps259538.vps.ovh.ca>
2019-09-19 10:13:11 -0400

    feat(administraProjScene): adiciona scene para administracao de projetos

---
+++ scenes/administraProjetos.js
@@ -0,0 +1,51 @@
+const { User } = require('../models/user.model');
+const Scene = require('telegraf/scenes/base')
+const { Markup } = require('telegraf');
+const axios = require('axios');
+const winston = require('winston');
+
+const administraProjScene = new Scene('administraProj');
+
+let projectId;
+
+administraProjScene.enter(async (ctx) => {
+    let userId = ctx.update.message.from.id;
+    let user = await User.findById(userId);
+    let projects = [];
+    user.projects.forEach(project => {
+        projects.push( Markup.callbackButton(project.nome, project._id));           
+    })
+    ctx.reply(
+        'Selecione um projeto:',
+        Markup.inlineKeyboard([projects]).extra()
+    )
+});
+
+administraProjScene.on('callback_query', async (ctx) => {
+    const projectId = ctx.update.callback_query;
+    const userId = ctx.from;
+    ctx.answerCbQuery('aguarde...');
+    if (ctx.update.callback_query.data == 'delete'){
+        let user = await User.findById(userId.id);
+        let index = user.projects.indexOf((obj) => obj._id == id);
+        user.projects.splice(index, 1);
+        user.save();
+        ctx.reply('Projeto excluído!');
+        return ctx.scene.leave();
+    }
+    if (ctx.update.callback_query.data == 'cancel'){
+        return ctx.scene.leave();
+    }
+    id = ctx.update.callback_query.data;
+    ctx.editMessageText(
+        'O que deseja fazer?',
+        Markup.inlineKeyboard([Markup.callbackButton('Excluir', 'delete'), Markup.callbackButton('Cancelar', 'cancel')]).extra()
+    )
+})
+
+administraProjScene.on('text', async (ctx) => {
+
+})
+
+
+module.exports = administraProjScene;
--- startup/scenes.js
+++ startup/scenes.js
@@ -2,9 +2,12 @@ const Stage = require('telegraf/stage')
 const { enter, leave } = Stage
 const lesgoScene = require('../scenes/lesgo.scene');
 const novoProjetoScene = require('../scenes/novoProjeto.scene');
+const administraProjScene = require('../scenes/administraProjetos');
 
 module.exports =  function(bot) {       
-    const stage = new Stage([lesgoScene, novoProjetoScene]);
+    const stage = new Stage([lesgoScene, novoProjetoScene, administraProjScene]);
     bot.use(stage.middleware());
     bot.command('lesgo', enter('lesgo'))
+    bot.command('projetos', enter('administraProj'))
+    bot.command('novoprojeto', enter('novoprojeto'))
 }
