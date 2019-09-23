2ec6ae44d0c5a46dfbc7a16f2df7adbc3eea46fa
root <itsadeadh2@gmail.com>
2019-09-23 19:48:55 +0000

    refac(oauth.controller):  cria controller que responde ao callback do oauth

--- controllers/oauth.controller.js
+++ controllers/oauth.controller.js
@@ -1,9 +1,15 @@
 const winston = require('winston');
 const oauthService = require('../services/oauth.service');
+const { User } = require('../models/user.model')
 
 const oauthController = {
     async process(req, res) {
-        let { code, state } = req.params;
-        winston.info({ code, state });
+        let { code, state } = req.query;
+        let user = await oauthService.process(state, code);
+        if (!user) return res.status(400).send('Ops! parece que há algo de errado com seu token. Tente novamente.');
+        winston.info(user);
+        res.send('Pronto, agora voce está autenticado no GitLab!')
     }
 }
+
+module.exports = oauthController;
