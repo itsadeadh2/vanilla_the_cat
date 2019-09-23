1134b79b7220ed5fcb65e09598783f79b13877d5
root <itsadeadh2@gmail.com>
2019-09-23 19:49:32 +0000

    feat(routes): cria rota para o callback da solicitacao oauth

---
+++ routes/express/oauth.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../../controllers/oauth.controller');
+
+router.get('/', controller.process);
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const route = require('../routes/express/webhook.routes');
+const webHookRoute = require('../routes/express/webhook.routes');
+const oauthRoute = require('../routes/express/oauth.routes');
 
 module.exports = function(app) {
     app.use(express.json());
-    app.use('/api/gitlabIntegration', route);
+    app.use('/api/gitlabIntegration', webHookRoute);
+    app.use('/api/oauth', oauthRoute);
 }
