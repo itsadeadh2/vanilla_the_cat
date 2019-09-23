90c733e4508e05af610ace3b829eaa88459f14e4
root <root@vps259538.vps.ovh.ca>
2019-09-23 09:26:00 -0400

    feat(oauth.service): cria servico para centralizar operacoes de oauth

---
+++ services/oauth.service.js
@@ -0,0 +1,22 @@
+const { User } = require('../models/user.model');
+const jwt  = require('jsonwebtoken');
+const config = require('config');
+
+const oauthService = {
+    async generateUserUrl(userId) {
+        let user = await User.findById(userId);
+        const token = jwt.sign({ _id: user._id, nome: user.nome  }, config.get('jwtPrivateKey'));
+        let client_id = 'b902d564260b4c23e661dd5392d555f1118e608fd322e6937541c7fc50b86730';
+        let redirectUri = encodeURIComponent('http://51.79.87.65:3000/api/oauth');
+        
+        let url = `https://gitlab.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUri}response_type=code&state=${token}&scope=api`
+        return url;
+    },
+
+    async validateToken(token) {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        let user = await User.findById(decoded._id);
+        if (!user) return false;
+        return true;
+    }
+}
