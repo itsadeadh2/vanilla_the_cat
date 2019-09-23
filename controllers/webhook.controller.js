cc5a2738eb707a5a2361910e518096a0da8c5022
root <root@vps259538.vps.ovh.ca>
2019-09-23 08:20:17 -0400

    refac(webhook.controller): transforma o webhook em um objeto concreto

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -1,9 +1,13 @@
 const winston = require('winston');
 const webhookService = require('../services/webhook.service');
 
-exports.post = async (req, res, next) => {
-    const { body } = req
-    await webhookService.process({ strategy: body.object_kind, data: body });
-    winston.info(body);
-    res.status(200).json(body);
+const webhookController = {
+    async notify(req, res) {
+        const { body } = req;
+        await webhookService.process({ strategy: body.object_kind, data: body });
+        winston.info(body);
+        res.status(200).json(body);
+    }
 }
+
+module.exports = webhookController;
