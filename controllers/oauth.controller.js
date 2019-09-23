d2b475f90e49ae915f0b6111c54ecdce4ee7d7d8
root <itsadeadh2@gmail.com>
2019-09-23 21:51:41 +0000

    refac(oaut.controller): utiliza variaveis proprias e remove dependencias nao utilizadas

--- controllers/oauth.controller.js
+++ controllers/oauth.controller.js
@@ -1,11 +1,10 @@
 const winston = require('winston');
 const oauthService = require('../services/oauth.service');
-const { User } = require('../models/user.model')
 
 const oauthController = {
     async process(req, res) {
-        let { code, state } = req.query;
-        let user = await oauthService.process(state, code);
+        const { code, state } = req.query;
+        const user = await oauthService.process(state, code);
         if (!user) return res.status(400).send('Ops! parece que há algo de errado com seu token. Tente novamente.');
         winston.info(user);
         res.send('Pronto, agora voce está autenticado no GitLab!')
