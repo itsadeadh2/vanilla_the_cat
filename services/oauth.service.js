e06a2bdaf9ae307b50681da1d2ae1e88e84ff849
root <itsadeadh2@gmail.com>
2019-09-23 17:29:53 +0000

    feat(oauth.service): cria metodo que busca o token do gitlab

--- services/oauth.service.js
+++ services/oauth.service.js
@@ -1,15 +1,22 @@
 const { User } = require('../models/user.model');
 const jwt  = require('jsonwebtoken');
 const config = require('config');
+const axios = require('axios');
 
 const oauthService = {
-    async generateUserUrl(userId) {
+    client_secret: 'meusecret',
+    client_id: 'minhaid',
+    redirectUri: encodeURIComponent('http://45.79.228.17:3000/api/oauth'),
+    baseUrl: 'https://gitlab.com/oauth',
+    
+    generateStateHash(user) {
+        return jwt.sign({ _id: user._id, nome: user.nome  }, config.get('jwtPrivateKey'));
+    },
+    
+    async generateUserUrl(userId) {        
         let user = await User.findById(userId);
-        const token = jwt.sign({ _id: user._id, nome: user.nome  }, config.get('jwtPrivateKey'));
-        let client_id = 'b902d564260b4c23e661dd5392d555f1118e608fd322e6937541c7fc50b86730';
-        let redirectUri = encodeURIComponent('http://51.79.87.65:3000/api/oauth');
-        
-        let url = `https://gitlab.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUri}response_type=code&state=${token}&scope=api`
+        let token = this.generateStateHash(user);
+        let url = `${this.baseUrl}/authorize?client_id=${this.client_id}&redirect_uri=${this.redirectUri}&response_type=code&state=${token}&scope=api`
         return url;
     },
 
@@ -17,7 +24,18 @@ const oauthService = {
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
         let user = await User.findById(decoded._id);
         if (!user) return false;
-        return true;
+        return user;
+    },    
+
+    async getUserToken(code) {
+        let url = `https://gitlab.com/oauth/token/`;
+        let res = await axios.post(url, {params: {
+            client_id: this.client_id,
+            client_secret: this.client_secret,
+            code: code,
+            grant_type: 'authorization_code',
+            redirect_uri: this.redirectUri
+        }});
     }
 }
 
