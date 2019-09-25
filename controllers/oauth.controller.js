56ecba40b41ebb9d6e31ed874d505340e1e03695
root <itsadeadh2@gmail.com>
2019-09-25 18:58:07 +0000

    fix(oauth.controller): lida com erros causados por requests feitos diretamente para o endpoint de oauth

--- controllers/oauth.controller.js
+++ controllers/oauth.controller.js
@@ -3,11 +3,15 @@ const { oauthService } = require('../services/oauth.service');
 
 const oauthController = {
   async process(req, res) {
-    const { code, state } = req.query;
-    const user = await oauthService.process(state, code);
-    if (!user) return res.status(400).send('Ops! parece que há algo de errado com seu token. Tente novamente.');
-    winston.info(user);
-    return res.send('Pronto, agora voce está autenticado no GitLab!');
+    try {
+      const { code, state } = req.query;
+      const user = await oauthService.process(state, code);
+      if (!user) return res.status(400).send('Ops! parece que há algo de errado com seu token. Tente novamente.');
+      winston.info(user);
+      return res.send('Pronto, agora voce está autenticado no GitLab!');
+    } catch (error) {
+      return res.status(400).send(error);
+    }
   },
 };
 
