83b6879c9b87e2ea893f0dab8e9a35f4815f5dcc
root <itsadeadh2@gmail.com>
2019-09-26 20:03:29 +0000

    fix(database): adiciona useUnidfiedTopology ao metodo de conexao

--- startup/database.js
+++ startup/database.js
@@ -4,6 +4,6 @@ const config = require('config');
 
 module.exports = () => {
   const db = config.get('db');
-  mongoose.connect(db, { useNewUrlParser: true })
+  mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => winston.info(`Connected to ${db} ...`));
 };
