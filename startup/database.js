4dee7f55c91a85083f1b632b383524499a78db39
root <itsadeadh2@gmail.com>
2019-09-26 13:18:44 +0000

    fix(database): adiciona newurlparser

--- startup/database.js
+++ startup/database.js
@@ -4,6 +4,6 @@ const config = require('config');
 
 module.exports = () => {
   const db = config.get('db');
-  mongoose.connect(db)
+  mongoose.connect(db, { useNewUrlParser: true })
     .then(() => winston.info(`Connected to ${db} ...`));
 };
